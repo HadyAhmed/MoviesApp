d490807cf76f76af78d6ab9fa8b7dc5d6c7d0bfd
hadi_ahmed <h.a7med2017@gmail.com>
2018-11-30 08:58:33 +0200

    Test Commit

--- app/src/main/java/com/hadi/movies/MainActivity.java
+++ app/src/main/java/com/hadi/movies/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String API_KEY = "8d87808da715fcc1f5da7f793310967d";
+    public static final String TOP_RATED = "https://api.themoviedb.org/3/movie/top_rated?api_key=8d87808da715fcc1f5da7f793310967d&language=en-US";
+    public static final String MOST_POPULAR = "https://api.themoviedb.org/3/movie/popular?api_key=8d87808da715fcc1f5da7f793310967d&language=en-US";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
